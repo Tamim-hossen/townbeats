09af52f1634688ad1b3d4032d64dd0637af50df0
Tamim <tamimhossen003@gmail.com>
2025-02-22 11:23:30 +0600

    style change

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
 import Banner from "@/components/Banner";
@@ -7,15 +7,20 @@ import NewsLetter from "@/components/NewsLetter";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useUser } from "@clerk/nextjs";
+
+
+
 
 const Home = () => {
+
   return (
     <>
       <Navbar/>
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
-        <HomeProducts />
         <FeaturedProduct />
+        <HomeProducts />
         <Banner />
         <NewsLetter />
       </div>
